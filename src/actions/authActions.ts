7afa463c8af01cd5f6b2a87a5874ec4666b5cbfc
King Elisha <ek.chibueze@gmail.com>
2020-02-19 21:20:22 +0100

    actions --> ts

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -1,15 +1,15 @@
-import axios from 'axios';
-import jwt_decode from 'jwt-decode';
-import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 // ===ACTIONS===
 
-export const getErrors = (errorData) => ({
+export const getErrors = (errorData: any) => ({
   type: GET_ERRORS,
   payload: errorData
 });
 
-export const setCurrentUser = (userData) => ({
+export const setCurrentUser = (userData: any) => ({
   type: SET_CURRENT_USER,
   payload: userData
 });
@@ -18,22 +18,26 @@ export const setCurrentUser = (userData) => ({
 
 // @action-type GET_ERRORS
 // @description sign-up user
-export const signupUser = (userData, history) => async (dispatch) => {
-  await axios.post('/api/users/signup', userData)
-    .then((res) => history.push('/signin'))
-    .catch((err) => {
+export const signupUser = (userData: any, history: any) => async (
+  dispatch: any
+) => {
+  await axios
+    .post("/api/users/signup", userData)
+    .then(res => history.push("/signin"))
+    .catch(err => {
       if (err.response) dispatch(getErrors(err.response.data));
     });
 };
 
 // @action-types SET_CURRENT_USER, GET_ERRORS
 // @description sign-in/authenticate user
-export const signinUser = (userData) => (dispatch) => {
-  axios.post("/api/users/signin", userData)
-    .then((res) => {
+export const signinUser = (userData: any) => (dispatch: any) => {
+  axios
+    .post("/api/users/signin", userData)
+    .then(res => {
       // save token to localStorage to enable global access
       const token = res.data.data;
-      localStorage.setItem('jwtToken', token);
+      localStorage.setItem("jwtToken", token);
 
       // add token to axios Authorization Header
       setAuthToken(token);
@@ -44,28 +48,27 @@ export const signinUser = (userData) => (dispatch) => {
       dispatch(setCurrentUser(decodedUserData));
       // window.location.href = "/home";
     })
-    .catch((err) => {
+    .catch(err => {
       if (err.response) dispatch(getErrors(err.response.data));
     });
-}
+};
 
 // @action-type SET_CURRENT_USER
 // @description sign-in/authenticate user
-export const signoutUser = (history) => (dispatch) => {
-  localStorage.removeItem('jwtToken');
-  setAuthToken(false);
+export const signoutUser = () => (dispatch: any) => {
+  localStorage.removeItem("jwtToken");
+  setAuthToken("");
   dispatch(setCurrentUser({}));
-}
-
+};
 
 // ===UTILS===
 // adds/deletes @token from the Authorization Header
-export const setAuthToken = (token) => {
+export const setAuthToken = (token: string) => {
   if (token) {
     // Apply token to every request
-    axios.defaults.headers.common['Authorization'] = token;
+    axios.defaults.headers.common["Authorization"] = token;
   } else {
     // Delete Auth Header
-    delete axios.defaults.headers.common['Authorization'];
+    delete axios.defaults.headers.common["Authorization"];
   }
-}
+};
