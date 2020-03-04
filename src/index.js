34a980274095bdb2b6088597afd7449f25298c09
King Elisha <ek.chibueze@gmail.com>
2020-03-04 22:57:08 +0100

    added new url

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import axios from "axios";
 // import app from 'firebase/app';
 // import { firebaseKeys } from './keys.env';
 
@@ -15,7 +16,9 @@ import * as serviceWorker from "./serviceWorker";
 //   messagingSenderId: firebaseKeys.FIREBASE_MESSEGING_SENDER_ID,
 // });
 
-ReactDOM.render((<App />), document.getElementById("root"));
+axios.defaults.baseURL = "https://blazehub-bk.herokuapp.com";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
