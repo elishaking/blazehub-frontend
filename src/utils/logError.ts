4a7b466291a5e9d300586efc0fe3ecf3fe6534e1
King Elisha <ek.chibueze@gmail.com>
2020-03-17 09:03:18 +0100

    updated logError and tslint rules

--- src/utils/logError.ts
+++ src/utils/logError.ts
@@ -1,5 +1,9 @@
 import { captureException } from "@sentry/browser";
 
-export default function logError(err: any) {
-  captureException(err);
+export default function logErrorFunction() {
+  if (process.env.NODE_ENV === "development") {
+    return console.error;
+  } else if (process.env.NODE_ENV === "production") {
+    return captureException;
+  }
 }
