f9d89827a98d74da723d143d6de4f67c0a86eeb7
King Elisha <ek.chibueze@gmail.com>
2020-03-22 12:25:06 +0100

    updated log-error

--- src/utils/logError.ts
+++ src/utils/logError.ts
@@ -1,9 +1,9 @@
 import { captureException } from "@sentry/browser";
 
 export default function logError(err: any) {
-  if (process.env.NODE_ENV === "development") {
-    console.error(err);
-  } else if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production") {
     captureException(err);
+  } else {
+    console.error(err);
   }
 }
