ef720df6c94fad0a88be7e200a6fa4e46abced9d
King Elisha <ek.chibueze@gmail.com>
2020-05-08 20:42:12 +0100

    form input

--- src/pages/Feedback.tsx
+++ src/pages/Feedback.tsx
@@ -1,12 +1,81 @@
 import React, { Component } from "react";
-import "./Feedback.scss";
+import { RouteComponentProps } from "react-router-dom";
+import { connect } from "react-redux";
 
-export default class Feedback extends Component {
+import "./Feedback.scss";
+import AuthNav from "../containers/nav/AuthNav";
+import MainNav from "../containers/nav/MainNav";
+import { AuthState } from "../models/auth";
+import {
+  TextFormInput,
+  TextAreaFormInput,
+} from "../components/form/TextFormInput";
+import { FeedbackErrors } from "../models/feedback";
+
+interface FeedbackProps extends RouteComponentProps {
+  auth: AuthState;
+}
+
+class Feedback extends Component<FeedbackProps> {
+  state = {
+    errors: {} as FeedbackErrors,
+  };
+
+  onChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   render() {
+    const { user } = this.props.auth;
+    const { errors } = this.state;
+
     return (
-      <div>
-        <h1>Feedback</h1>
+      <div className="container">
+        <AuthNav showSearch={true} history={this.props.history} />
+
+        <div className="main">
+          <MainNav user={user} />
+
+          <form onSubmit={this.onSubmit}>
+            <TextFormInput
+              type="text"
+              name="name"
+              placeholder="name"
+              error={errors.name}
+              onChange={this.onChange}
+            />
+
+            <TextFormInput
+              type="email"
+              name="email"
+              placeholder="email"
+              error={errors.email}
+              onChange={this.onChange}
+            />
+
+            <TextAreaFormInput
+              name="message"
+              placeholder="message"
+              error={errors.message}
+              onChange={this.onChange}
+            />
+          </form>
+        </div>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state: any) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(Feedback);
