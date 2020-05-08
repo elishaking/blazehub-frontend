29c66a78934248a701fab786b772b2153c8301d2
King Elisha <ek.chibueze@gmail.com>
2020-05-08 20:29:06 +0100

    feedback component

---
+++ src/pages/Feedback.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Feedback.scss";
+
+export default class Feedback extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Feedback</h1>
+      </div>
+    );
+  }
+}
