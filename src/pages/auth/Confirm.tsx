e271a8b01543a2244d104aed3c1a392c27417c5e
King Elisha <ek.chibueze@gmail.com>
2020-05-21 20:11:17 +0100

    auth-container

--- src/pages/auth/Confirm.tsx
+++ src/pages/auth/Confirm.tsx
@@ -5,9 +5,9 @@ import { RouteComponentProps, match } from "react-router-dom";
 import { verifyConfirmToken } from "../../actions/authActions";
 import Spinner from "../../components/Spinner";
 import { AuthState } from "../../models/auth";
-import Logo from "../../components/Logo";
 import "./Landing.scss";
 import logError from "../../utils/logError";
+import AuthContainer from "./AuthContainer";
 
 interface ConfirmProps extends RouteComponentProps {
   auth: AuthState;
@@ -15,7 +15,6 @@ interface ConfirmProps extends RouteComponentProps {
 }
 
 interface ConfirmState {
-  navLogoColor: string | undefined;
   loading: boolean;
   message: string;
   successful: boolean;
@@ -24,9 +23,8 @@ interface ConfirmState {
 class Confirm extends Component<ConfirmProps, Readonly<ConfirmState>> {
   constructor(props: ConfirmProps) {
     super(props);
-    console.log(props.match.params.token);
+
     this.state = {
-      navLogoColor: "#fff",
       loading: true,
       message: "",
       successful: false,
@@ -34,9 +32,6 @@ class Confirm extends Component<ConfirmProps, Readonly<ConfirmState>> {
   }
 
   componentDidMount() {
-    this.resize();
-    window.addEventListener("resize", this.resize);
-
     // if user is already authenticated, redirect to dashboard
     if (this.props.auth.isAuthenticated) {
       this.props.history.push("/home");
@@ -61,10 +56,6 @@ class Confirm extends Component<ConfirmProps, Readonly<ConfirmState>> {
       });
   }
 
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.resize);
-  }
-
   // after redux store is updated, this life cycle method will be called
   componentWillReceiveProps(nextProps: any) {
     this.redirectIfAuthenticated(nextProps.auth.isAuthenticated);
@@ -83,15 +74,6 @@ class Confirm extends Component<ConfirmProps, Readonly<ConfirmState>> {
     }
   };
 
-  resize = () => {
-    const newLogoColor = window.innerWidth > 1000 ? "#fff" : undefined;
-    if (this.state.navLogoColor !== newLogoColor) {
-      this.setState({
-        navLogoColor: newLogoColor,
-      });
-    }
-  };
-
   onChange = (event: any) => {
     this.setState({
       [event.target.name]: event.target.value,
@@ -103,96 +85,43 @@ class Confirm extends Component<ConfirmProps, Readonly<ConfirmState>> {
   };
 
   render() {
-    const { navLogoColor, loading, message, successful } = this.state;
+    const { loading, message, successful } = this.state;
     // console.log(errors);
 
     return (
-      <div className="container landing-bg">
-        <header>
-          <nav>
-            <h1>
-              <Logo color={navLogoColor} style={{ fontSize: "1.3em" }} />
-              <span>BlazeHub</span>
+      <AuthContainer>
+        {loading ? (
+          <div
+            style={{
+              display: "block",
+              width: "100%",
+              textAlign: "center",
+            }}
+          >
+            <Spinner />
+            <small>Verifying confirmation url</small>
+          </div>
+        ) : (
+          <div>
+            <h1 style={{ color: successful ? undefined : "#ca0000" }}>
+              {message}
             </h1>
-          </nav>
-        </header>
-
-        <div className="content">
-          <div className="left">
-            <div className="inner">
-              <div className="info">
-                <ul>
-                  <li>
-                    <img
-                      src="../assets/img/connect.svg"
-                      alt="Connection"
-                      srcSet=""
-                    />
-                    <h2>Connect With Friends</h2>
-                  </li>
-                  <li>
-                    <img
-                      src="../assets/img/converse.svg"
-                      alt="Conversation"
-                      srcSet=""
-                    />
-                    <h2>Chat, Share Photos, Videos and more</h2>
-                  </li>
-                  <li>
-                    <img
-                      src="../assets/img/commune.svg"
-                      alt="Community"
-                      srcSet=""
-                    />
-                    <h2>Be a part of a growing community</h2>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            <br></br>
+            {successful ? (
+              <button className="btn" onClick={(e) => this.navigate("/signin")}>
+                Sign In
+              </button>
+            ) : (
+              <button
+                className="btn"
+                onClick={(e) => this.navigate("/confirm/resend")}
+              >
+                Resend URL
+              </button>
+            )}
           </div>
-
-          <div className="right">
-            <div className="inner">
-              <div className="welcome">
-                {loading ? (
-                  <div
-                    style={{
-                      display: "block",
-                      width: "100%",
-                      textAlign: "center",
-                    }}
-                  >
-                    <Spinner />
-                    <small>Verifying confirmation url</small>
-                  </div>
-                ) : (
-                  <div>
-                    <h1 style={{ color: successful ? undefined : "#ca0000" }}>
-                      {message}
-                    </h1>
-                    <br></br>
-                    {successful ? (
-                      <button
-                        className="btn"
-                        onClick={(e) => this.navigate("/signin")}
-                      >
-                        Sign In
-                      </button>
-                    ) : (
-                      <button
-                        className="btn"
-                        onClick={(e) => this.navigate("/confirm/resend")}
-                      >
-                        Resend URL
-                      </button>
-                    )}
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        )}
+      </AuthContainer>
     );
   }
 }
