b24f7195fff7a6aca29ea72a580c80214d6ab2b4
King Elisha <ek.chibueze@gmail.com>
2020-07-07 09:23:36 +0100

    refactor: update <Posts /> render

--- src/containers/Posts.tsx
+++ src/containers/Posts.tsx
@@ -4,6 +4,7 @@ import app from "firebase/app";
 import "firebase/database";
 import Post from "../components/Post";
 import Spinner from "../components/Spinner";
+import { PostData } from "../models/post";
 
 interface PostsProps extends RouteComponentProps {
   user: any;
@@ -13,7 +14,7 @@ interface PostsProps extends RouteComponentProps {
 }
 
 interface PostsState {
-  posts: any[];
+  posts: PostData[];
   loadingPosts: boolean;
 }
 
@@ -29,7 +30,7 @@ class Posts extends Component<PostsProps, Readonly<PostsState>> {
 
     this.state = {
       posts: [],
-      loadingPosts: true
+      loadingPosts: true,
     };
 
     this.db = app.database();
@@ -47,7 +48,7 @@ class Posts extends Component<PostsProps, Readonly<PostsState>> {
       this.postsRef
         .orderByChild("user/id")
         .equalTo(this.otherUser ? otherUserId : this.user.id)
-        .once("value", postsSnapShot => {
+        .once("value", (postsSnapShot) => {
           const posts = postsSnapShot.val() || {};
 
           this.setState({
@@ -55,7 +56,7 @@ class Posts extends Component<PostsProps, Readonly<PostsState>> {
               const postKey = postKeys[postKeys.length - i - 1];
               const newPost = {
                 key: postKey,
-                ...posts[postKey]
+                ...posts[postKey],
               };
               // set date
               newPost.date = 1e15 - newPost.date;
@@ -64,13 +65,13 @@ class Posts extends Component<PostsProps, Readonly<PostsState>> {
                 this.setState({ loadingPosts: false });
 
               return newPost;
-            })
+            }),
           });
 
           if (this.state.loadingPosts) this.setState({ loadingPosts: false });
         });
 
-      this.postsRef.on("child_removed", postSnapShot => {
+      this.postsRef.on("child_removed", (postSnapShot) => {
         const { posts } = this.state;
 
         const deleteIndex = posts.findIndex(
@@ -80,43 +81,45 @@ class Posts extends Component<PostsProps, Readonly<PostsState>> {
         this.setState({ posts });
       });
     } else {
-      this.postsRef.orderByChild("date").on("child_added", newPostSnapShot => {
-        // console.log('child_added');
-        const newPost = {
-          key: newPostSnapShot.key,
-          ...newPostSnapShot.val()
-        };
+      this.postsRef
+        .orderByChild("date")
+        .on("child_added", (newPostSnapShot) => {
+          // console.log('child_added');
+          const newPost = {
+            key: newPostSnapShot.key,
+            ...newPostSnapShot.val(),
+          };
 
-        // update imageUrl
-        // if (newPost.imageUrl && newPost.imageUrl !== true) {
-        //   this.db.ref("post-images").child(newPost.key).set(newPost.imageUrl);
-        //   this.postsRef.child(newPost.key).child("imageUrl").set(true);
-        // }
+          // update imageUrl
+          // if (newPost.imageUrl && newPost.imageUrl !== true) {
+          //   this.db.ref("post-images").child(newPost.key).set(newPost.imageUrl);
+          //   this.postsRef.child(newPost.key).child("imageUrl").set(true);
+          // }
 
-        // update date
-        // if (newPost.date < 1e+13) {
-        //   this.postsRef.child(newPost.key).child("date").set(1e+15 - newPost.date);
-        // }
+          // update date
+          // if (newPost.date < 1e+13) {
+          //   this.postsRef.child(newPost.key).child("date").set(1e+15 - newPost.date);
+          // }
 
-        // set date
-        newPost.date = 1e15 - newPost.date;
+          // set date
+          newPost.date = 1e15 - newPost.date;
 
-        if (this.state.loadingPosts) this.setState({ loadingPosts: false });
+          if (this.state.loadingPosts) this.setState({ loadingPosts: false });
 
-        const { posts } = this.state;
-        newPost.date > this.mountedOn
-          ? posts.unshift(newPost)
-          : posts.push(newPost);
-        this.setState({
-          posts
+          const { posts } = this.state;
+          newPost.date > this.mountedOn
+            ? posts.unshift(newPost)
+            : posts.push(newPost);
+          this.setState({
+            posts,
+          });
         });
-      });
 
-      this.postsRef.on("child_removed", removedPostSnapShot => {
+      this.postsRef.on("child_removed", (removedPostSnapShot) => {
         const { posts } = this.state;
 
         posts.splice(
-          posts.map(post => post.key).indexOf(removedPostSnapShot.key),
+          posts.findIndex((post) => post.key === removedPostSnapShot.key),
           1
         );
 
@@ -129,52 +132,52 @@ class Posts extends Component<PostsProps, Readonly<PostsState>> {
     const { loadingPosts, posts } = this.state;
     // const { avatar } = this.props;
 
+    if (loadingPosts)
+      return (
+        <div className="loading-container">
+          <Spinner />
+        </div>
+      );
+
+    if (posts.length > 0)
+      return posts.map((post: any) => (
+        <Post
+          key={post.key}
+          // profilesRef={app.database().ref("profiles")}
+          postRef={this.postsRef.child(post.key)}
+          postImageRef={this.db.ref("post-images").child(post.key)}
+          profilePhotosRef={this.db.ref("profile-photos")}
+          bookmarkRef={this.db
+            .ref("bookmarks")
+            .child(this.user.id)
+            .child(post.key)}
+          notificationsRef={app.database().ref("notifications")}
+          post={post}
+          user={this.user}
+          canBookmark={true}
+          otherUser={this.otherUser}
+        />
+      ));
+
     return (
-      <React.Fragment>
-        {loadingPosts ? (
-          <div className="loading-container">
-            <Spinner />
-          </div>
-        ) : posts.length > 0 ? (
-          posts.map((post: any) => (
-            <Post
-              key={post.key}
-              // profilesRef={app.database().ref("profiles")}
-              postRef={this.postsRef.child(post.key)}
-              postImageRef={this.db.ref("post-images").child(post.key)}
-              profilePhotosRef={this.db.ref("profile-photos")}
-              bookmarkRef={this.db
-                .ref("bookmarks")
-                .child(this.user.id)
-                .child(post.key)}
-              notificationsRef={app.database().ref("notifications")}
-              post={post}
-              user={this.user}
-              canBookmark={true}
-              otherUser={this.otherUser}
-            />
-          ))
-        ) : (
-          <div
-            className="loading-container"
-            style={{
-              padding: "1em",
-              textAlign: "center"
-            }}
-          >
-            <p>You have not created any Posts yet</p>
-            <button
-              className="btn"
-              style={{
-                marginTop: "1em"
-              }}
-              onClick={() => this.props.history.push("/home")}
-            >
-              Create Post
-            </button>
-          </div>
-        )}
-      </React.Fragment>
+      <div
+        className="loading-container"
+        style={{
+          padding: "1em",
+          textAlign: "center",
+        }}
+      >
+        <p>You have not created any Posts yet</p>
+        <button
+          className="btn"
+          style={{
+            marginTop: "1em",
+          }}
+          onClick={() => this.props.history.push("/home")}
+        >
+          Create Post
+        </button>
+      </div>
     );
   }
 }
