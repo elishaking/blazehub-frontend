47f9f99bb3c6d71cb2202226560511e4b1bbf23b
King Elisha <ek.chibueze@gmail.com>
2020-07-21 09:43:28 +0100

    refactor(<Profile />): re-ordered methods

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -122,229 +122,6 @@ class Profile extends Component<ProfileProps, Readonly<any>> {
     }
   }
 
-  setPic = (key: string, dataUrl: string) => {
-    key === "avatar"
-      ? this.setState({ avatar: dataUrl, loadingAvatar: false })
-      : this.setState({ coverPhoto: dataUrl, loadingCoverPhoto: false });
-  };
-
-  setFriends = (friendKeys: string[], friends: any) => {
-    this.setState({
-      friends: friendKeys.map((friendKey) => ({
-        key: friendKey,
-        ...friends[friendKey],
-      })),
-    });
-  };
-
-  loadOtherUserFriends = () => {
-    this.db
-      .ref("friends")
-      .child(this.otherUserId)
-      .once("value")
-      .then((friendsSnapShot) => {
-        if (friendsSnapShot.exists()) {
-          const friends = friendsSnapShot.val();
-          this.setFriends(Object.keys(friends), friends);
-        }
-      });
-  };
-
-  loadOtherUserProfilePhotos = () => {
-    this.db
-      .ref("profile-photos")
-      .child(this.otherUserId)
-      .once("value")
-      .then((photosSnapShot) => {
-        const photos = photosSnapShot.exists()
-          ? photosSnapShot.val()
-          : { avatar: "", coverPhoto: "" };
-        this.setPic("avatar", photos.avatar);
-        this.setPic("coverPhoto", photos.coverPhoto);
-      });
-  };
-
-  loadOtherUserProfileData = () => {
-    this.db
-      .ref("profiles")
-      .orderByChild("username")
-      .equalTo(this.props.match.params.username)
-      .once("value")
-      .then((profileSnapShot) => {
-        if (!profileSnapShot.exists()) return (window.location.href = "/home");
-
-        const profile = profileSnapShot.val();
-        this.otherUserId = Object.keys(profile)[0];
-        this.setProfile(profile[this.otherUserId]);
-        // console.log(profileSnapShot.val())
-        this.setState({ loadingOtherUserId: false });
-
-        this.loadOtherUserFriends();
-
-        this.loadOtherUserProfilePhotos();
-      });
-  };
-
-  loadUserProfileData = () => {
-    const { user } = this.props.auth;
-    const { friends, profile } = this.props;
-
-    const friendKeys = Object.keys(friends);
-    friendKeys.length === 0
-      ? this.props.getFriends()
-      : this.setFriends(friendKeys, friends);
-
-    const avatar = profile.avatar;
-    avatar
-      ? this.setPic("avatar", avatar)
-      : this.props.getProfilePic(user.id, "avatar");
-    const coverPhoto = profile.coverPhoto;
-    coverPhoto
-      ? this.setPic("coverPhoto", coverPhoto)
-      : this.props.getProfilePic(user.id, "coverPhoto");
-
-    // createProfileForExistingUser();
-
-    this.profileRef.once("value", (profileSnapShot) => {
-      this.setProfile(profileSnapShot.val());
-    });
-  };
-
-  /**
-   * @param {ProfileData} profile
-   */
-  setProfile = (profile: ProfileData) => {
-    this.setState({
-      loadingProfile: false,
-      username: profile.username,
-      name: profile.name,
-      bio: profile.bio,
-      location: profile.location,
-      website: profile.website,
-      birth: profile.birth,
-    });
-  };
-
-  selectCoverPhoto = () => {
-    this.updateCover = true;
-    (document.getElementById("img-input") as HTMLElement).click();
-  };
-
-  selectAvatar = () => {
-    this.updateCover = false;
-    (document.getElementById("img-input") as HTMLElement).click();
-  };
-
-  /** @param {React.ChangeEvent<HTMLInputElement>} e */
-  processPic = (e: any) => {
-    const imgInput = e.target;
-    if (imgInput.files && imgInput.files[0]) {
-      const imgReader = new FileReader();
-      // const key = this.updateCover ? "coverPhoto" : "avatar";
-
-      imgReader.onload = (err: any) => {
-        if (imgInput.files[0].size > 100000)
-          resizeImage(
-            err.target.result.toString(),
-            imgInput.files[0].type
-          ).then((dataUrl: any) => {
-            if (this.updateCover) {
-              this.updatePic(dataUrl);
-            } else {
-              resizeImage(
-                err.target.result.toString(),
-                imgInput.files[0].type,
-                50
-              ).then((dataUrlSmall: any) => {
-                this.updatePic(dataUrl, dataUrlSmall);
-              });
-            }
-          });
-        else this.updatePic(err.target.result);
-      };
-
-      imgReader.readAsDataURL(imgInput.files[0]);
-    }
-  };
-
-  updatePic = (dataUrl: string, dataUrlSmall = "") => {
-    if (this.updateCover) {
-      this.setState({ loadingCoverPhoto: true });
-      this.props.updateProfilePic(
-        this.props.auth.user.id,
-        "coverPhoto",
-        dataUrl
-      );
-    } else {
-      this.setState({ loadingAvatar: true });
-      this.props.updateProfilePic(
-        this.props.auth.user.id,
-        "avatar",
-        dataUrl,
-        dataUrlSmall
-      );
-    }
-  };
-
-  /** @param {any} e */
-  onChange = (e: any) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  toggleEditProfile = () => {
-    this.setState({ editProfile: !this.state.editProfile });
-  };
-
-  /** @param {React.FormEvent<HTMLFormElement>} e */
-  editProfile = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    this.setState({ loadingProfile: true });
-
-    const { name, bio, location, website, birth } = this.state;
-
-    const { isValid, errors } = validateProfileEditInput({
-      name,
-      bio,
-      location,
-      website,
-      birth,
-    });
-
-    // console.log({ isValid, errors });
-
-    this.setState({ errors });
-
-    if (isValid) {
-      this.profileRef.update(
-        {
-          name,
-          bio,
-          location: location || "",
-          website: website || "",
-          birth: birth || "",
-        },
-        (err) => {
-          this.setState({ loadingProfile: false });
-
-          if (err) {
-            // console.log(err);
-            return;
-          }
-
-          this.toggleEditProfile();
-        }
-      );
-    } else {
-      this.setState({ loadingProfile: false });
-    }
-  };
-
-  findFriends = () => {
-    this.props.history.push("/find");
-  };
-
   render() {
     const { user } = this.props.auth;
     const {
@@ -646,6 +423,225 @@ class Profile extends Component<ProfileProps, Readonly<any>> {
       </div>
     );
   }
+
+  setPic = (key: string, dataUrl: string) => {
+    key === "avatar"
+      ? this.setState({ avatar: dataUrl, loadingAvatar: false })
+      : this.setState({ coverPhoto: dataUrl, loadingCoverPhoto: false });
+  };
+
+  setFriends = (friendKeys: string[], friends: any) => {
+    this.setState({
+      friends: friendKeys.slice(0, 7).map((friendKey) => ({
+        key: friendKey,
+        ...friends[friendKey],
+      })),
+    });
+  };
+
+  loadOtherUserFriends = () => {
+    this.db
+      .ref("friends")
+      .child(this.otherUserId)
+      .once("value")
+      .then((friendsSnapShot) => {
+        if (friendsSnapShot.exists()) {
+          const friends = friendsSnapShot.val();
+          this.setFriends(Object.keys(friends), friends);
+        }
+      });
+  };
+
+  loadOtherUserProfilePhotos = () => {
+    this.db
+      .ref("profile-photos")
+      .child(this.otherUserId)
+      .once("value")
+      .then((photosSnapShot) => {
+        const photos = photosSnapShot.exists()
+          ? photosSnapShot.val()
+          : { avatar: "", coverPhoto: "" };
+        this.setPic("avatar", photos.avatar);
+        this.setPic("coverPhoto", photos.coverPhoto);
+      });
+  };
+
+  loadOtherUserProfileData = () => {
+    this.db
+      .ref("profiles")
+      .orderByChild("username")
+      .equalTo(this.props.match.params.username)
+      .once("value")
+      .then((profileSnapShot) => {
+        if (!profileSnapShot.exists()) return (window.location.href = "/home");
+
+        const profile = profileSnapShot.val();
+        this.otherUserId = Object.keys(profile)[0];
+        this.setProfile(profile[this.otherUserId]);
+        // console.log(profileSnapShot.val())
+        this.setState({ loadingOtherUserId: false });
+
+        this.loadOtherUserFriends();
+
+        this.loadOtherUserProfilePhotos();
+      });
+  };
+
+  loadUserProfileData = () => {
+    const { user } = this.props.auth;
+    const { friends, profile } = this.props;
+
+    const friendKeys = Object.keys(friends);
+    friendKeys.length === 0
+      ? this.props.getFriends()
+      : this.setFriends(friendKeys, friends);
+
+    const avatar = profile.avatar;
+    avatar
+      ? this.setPic("avatar", avatar)
+      : this.props.getProfilePic(user.id, "avatar");
+    const coverPhoto = profile.coverPhoto;
+    coverPhoto
+      ? this.setPic("coverPhoto", coverPhoto)
+      : this.props.getProfilePic(user.id, "coverPhoto");
+
+    // createProfileForExistingUser();
+
+    this.profileRef.once("value", (profileSnapShot) => {
+      this.setProfile(profileSnapShot.val());
+    });
+  };
+
+  setProfile = (profile: ProfileData) => {
+    this.setState({
+      loadingProfile: false,
+      username: profile.username,
+      name: profile.name,
+      bio: profile.bio,
+      location: profile.location,
+      website: profile.website,
+      birth: profile.birth,
+    });
+  };
+
+  selectCoverPhoto = () => {
+    this.updateCover = true;
+    (document.getElementById("img-input") as HTMLElement).click();
+  };
+
+  selectAvatar = () => {
+    this.updateCover = false;
+    (document.getElementById("img-input") as HTMLElement).click();
+  };
+
+  processPic = (e: any) => {
+    const imgInput = e.target;
+    if (imgInput.files && imgInput.files[0]) {
+      const imgReader = new FileReader();
+      // const key = this.updateCover ? "coverPhoto" : "avatar";
+
+      imgReader.onload = (err: any) => {
+        if (imgInput.files[0].size > 100000)
+          resizeImage(
+            err.target.result.toString(),
+            imgInput.files[0].type
+          ).then((dataUrl: any) => {
+            if (this.updateCover) {
+              this.updatePic(dataUrl);
+            } else {
+              resizeImage(
+                err.target.result.toString(),
+                imgInput.files[0].type,
+                50
+              ).then((dataUrlSmall: any) => {
+                this.updatePic(dataUrl, dataUrlSmall);
+              });
+            }
+          });
+        else this.updatePic(err.target.result);
+      };
+
+      imgReader.readAsDataURL(imgInput.files[0]);
+    }
+  };
+
+  updatePic = (dataUrl: string, dataUrlSmall = "") => {
+    if (this.updateCover) {
+      this.setState({ loadingCoverPhoto: true });
+      this.props.updateProfilePic(
+        this.props.auth.user.id,
+        "coverPhoto",
+        dataUrl
+      );
+    } else {
+      this.setState({ loadingAvatar: true });
+      this.props.updateProfilePic(
+        this.props.auth.user.id,
+        "avatar",
+        dataUrl,
+        dataUrlSmall
+      );
+    }
+  };
+
+  /** @param {any} e */
+  onChange = (e: any) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  toggleEditProfile = () => {
+    this.setState({ editProfile: !this.state.editProfile });
+  };
+
+  /** @param {React.FormEvent<HTMLFormElement>} e */
+  editProfile = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    this.setState({ loadingProfile: true });
+
+    const { name, bio, location, website, birth } = this.state;
+
+    const { isValid, errors } = validateProfileEditInput({
+      name,
+      bio,
+      location,
+      website,
+      birth,
+    });
+
+    // console.log({ isValid, errors });
+
+    this.setState({ errors });
+
+    if (isValid) {
+      this.profileRef.update(
+        {
+          name,
+          bio,
+          location: location || "",
+          website: website || "",
+          birth: birth || "",
+        },
+        (err) => {
+          this.setState({ loadingProfile: false });
+
+          if (err) {
+            // console.log(err);
+            return;
+          }
+
+          this.toggleEditProfile();
+        }
+      );
+    } else {
+      this.setState({ loadingProfile: false });
+    }
+  };
+
+  findFriends = () => {
+    this.props.history.push("/find");
+  };
 }
 
 const mapStateToProps = (state: any) => ({
